2794f41b0f0b00981293fdf979eab49aeff21788
belgrencc <b_elgren@coloradocollege.edu>
2018-07-27 22:05:45 -0700

    Add task based on input

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -1,12 +1,39 @@
 import React from "react";
 
 class InputLine extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      typedText: ""
+    }
+  }
+
+  handleTyping(event){
+    this.setState({
+      typedText: event.target.value
+    })
+  }
+
+  handleSubmit(event){
+    if (this.state.typedText){
+      this.props.submit(event, this.state.typedText);
+      this.setState({
+        typedText: ""
+      })
+    } else{
+      event.preventDefault();
+    }
+  }
+
   render(){
     return (
       <form>
-        <input type="text"></input>
+        <input type="text" value={this.state.typedText} onChange={
+          (event) => this.handleTyping(event)
+        }>
+        </input>
         <button type="submit" onClick={
-          (event) => this.props.submit(event, "tester2")
+          (event) => this.handleSubmit(event)//this.props.submit(event, "tester2")
         }>
         Add Todo</button>
       </form>
