abb2da8a5c0056079e0b1ab669ffa7e907de248f
belgrencc <b_elgren@coloradocollege.edu>
2018-07-28 19:00:06 -0700

    changing repo name

--- reactApp/components/TodoApp.js
+++ reactApp/components/TodoApp.js
@@ -35,7 +35,7 @@ class TodoApp extends React.Component{
     dummyData = dummyData.filter((item) => item != todo)
     this.setState({
       todos: dummyData
-    }) 
+    })
   }
 
   toggleStrike(event, todo){
@@ -51,7 +51,7 @@ class TodoApp extends React.Component{
   }
 
   render(){
-    return (
+    return ( 
       <div>
         <InputLine
           submit={(event, task) => this.addTodo(event, task)}
